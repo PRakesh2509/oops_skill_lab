b28b0eb2234e680214a24f86813d87bd12082f8b
PRakesh2509 <rakeshpavuluri999@gmail.com>
2025-08-19 12:12:56 +0530

    Add files via upload

---
+++ SubstringExample1.java
@@ -0,0 +1,8 @@
+import java.time.*;
+public class SubstringExample1 {
+	public static void main(String[] args) {
+        String s1 = "JavaProgramming";
+        String sub = s1.substring(4, 11); 
+        System.out.println("Substring: " + sub);
+    }
+}
